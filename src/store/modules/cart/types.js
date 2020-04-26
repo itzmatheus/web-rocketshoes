29541d3b922a43668379d14f053341860fb0842b
Matheus Leite <matheusjoselfm@gmail.com>
2020-04-25 21:25:25 -0300

    Adicionando react toastify e refatorando reducer para verificar quantidade de produto em estoque ao adicionar

--- src/store/modules/cart/types.js
+++ src/store/modules/cart/types.js
@@ -1,4 +1,4 @@
 export const ADD_TO_CART_REQUEST = '@cart/ADD_REQUEST';
 export const ADD_TO_CART_SUCCESS = '@cart/ADD_SUCCESS';
 export const REMOVE_FROM_CART = '@cart/REMOVE';
-export const UPDATE_AMOUNT = '@cart/UpdateAmount';
+export const UPDATE_AMOUNT = '@cart/UPDATE_AMOUNT';
